37ba63000c38e806fc45c88ac03abb2aed986249
Kisan Patel <kpate262@uic.edu>
2019-12-13 12:25:03 -0600

    canvas

--- canvas.js
+++ canvas.js
@@ -1,10 +1,15 @@
 function draw(){
+  var background = new Image();
+  background.src = "./red.jpg";
   var canvas = document.getElementById('canvas');
+  ctx.drawImage(background, 0, 0);
   if (canvas.getContext) {
      var ctx = canvas.getContext('2d');
 
     ctx.beginPath();
+    ctx.fillStyle = "blue";
     ctx.arc(75, 75, 50, 0, Math.PI * 2, true); // Outer circle
+
     ctx.fill();
   }
 }
